064ad272c8f7d8becef10ae88b1bcab86060dd2a
Jonathan Ayten (aytenj) <aytenj@coventry.ac.uk>
2019-12-06 17:37:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,33 @@ var express = require('express');
 //create an express app
 var app = express();
 
+const path = require('path');
+app.use(express.static('public'));
+
  //add a callback function to handle
  //get request on the root
 app.get('/', function(req, res) {
+ res.sendFile(path.join(dirname+'/Webpages -HTML/Homepage.html'));
+ 
+});
+app.get('/', function(req, res) {
+ res.sendFile(path.join(dirname+'/Webpages -HTML/Products.html'));
+ 
+});
 
- //save the html code in a variable
-var homePage = "<h1 style='color:blue'>Welcome to Crush Games</h1>";
- //send the html code to the client
-res.send(homePage);
- });
+app.get('/', function(req, res) {
+ res.sendFile(path.join(dirname+'/Webpages -HTML/Our Team.html'));
+ 
+});
+ 
+app.get('/', function(req, res) {
+ res.sendFile(path.join(dirname+'/Webpages -HTML/Contact Us.html'));
+ 
+});
+ 
+app.get('/', function(req, res) {
+ res.sendFile(path.join(dirname+'/Webpages -HTML/Admin Login.html'));
+ 
+});
  //run the server on port 3000 
 app.listen(3000);
